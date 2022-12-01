cd43e06c486dc1ac1c8a145c5fb742ba55d62c5f
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-12-01 23:07:49 +0400

    main

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a697a94b7emshc12554e90c0792cp19610djsnda376982bdd6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b4fdf45e697b9201db2ee939220898b1";
